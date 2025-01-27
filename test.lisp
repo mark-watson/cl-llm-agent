68f272f3fa5448853074a26466444c427a0745b5
Mark Watson <markw@markwatson.com>
2025-01-27 16:10:56 -0700

    -

--- test.lisp
+++ test.lisp
@@ -34,3 +34,5 @@
 ;;(cl-llm-agent:agent-converse my-agent "Read the file 'README.md' in the current directory.")
 
 ;;(cl-llm-agent:agent-converse my-agent "Read the file 'test.txt' in the current directory and summarize the text in the file.")
+
+(cl-llm-agent:agent-converse my-agent "Search the web to find information on AI advancements and then summarize the search results.")
