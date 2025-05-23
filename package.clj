72f431330c2ae220ed1276b8dfa45f94cf264649
Mark Watson <markw@markwatson.com>
2025-05-23 16:15:43 -0700

    -

--- package.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-llm-agent.core
-  (:doc "Core namespace for the Clojure LLM Agent framework."))
-
-;; Later, we will add :require clauses for other namespaces
-;; and ensure necessary functions and macros are public.
-;; For now, this defines the main namespace.
